8e264ffe1add27f6e216fe5bd5bbf90b3dd3d2a5
Randy Zwitch <rzwitch+rsitecatalyst@gmail.com>
2015-07-11 18:31:58 +0000

    version 1.4.4

--- R/RSiteCatalyst.R
+++ R/RSiteCatalyst.R
@@ -1,14 +1,14 @@
 #' Package:  RSiteCatalyst \cr
 #' Type:     Package \cr
-#' Version:  1.4.3 \cr
-#' Date:     2015-01-06 \cr
+#' Version:  1.4.4 \cr
+#' Date:     2015-07-11 \cr
 #' License:  MIT + file LICENSE \cr
 #'
 #'
 #' 
 #' @name RSiteCatalyst
 #' @docType package
-#' @title R Client for Adobe Analytics API v1.4 
+#' @title R Client for Adobe Analytics API V1.4 
 #' @author Willem Paling, Randy Zwitch, Jowanza Joseph
 #' @description 
 #' This package contains an "analyst's toolbox" of functions for accessing the Adobe Analytics Reporting API v1.4. 

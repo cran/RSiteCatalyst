e4663c8a8b52dc5a6540c75a2b20bc2e03132a30
Randy Zwitch <rzwitch+rsitecatalyst@gmail.com>
2016-06-22 05:42:09 +0000

    version 1.4.9

--- R/RSiteCatalyst.R
+++ R/RSiteCatalyst.R
@@ -1,7 +1,7 @@
 #' Package:  RSiteCatalyst \cr
 #' Type:     Package \cr
-#' Version:  1.4.8 \cr
-#' Date:     2016-04-03 \cr
+#' Version:  1.4.9 \cr
+#' Date:     2016-06-21 \cr
 #' License:  MIT + file LICENSE \cr
 #'
 #'
